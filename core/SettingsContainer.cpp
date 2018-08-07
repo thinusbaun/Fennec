f942e6b4f68f010d3eb3721de63be1103196d475
Michał Katner <katnerster@gmail.com>
2018-08-07 20:55:32 +0200

    #1 SettingsContainer

--- core/SettingsContainer.cpp
+++ core/SettingsContainer.cpp
@@ -2,6 +2,53 @@
 
 SettingsContainer::SettingsContainer() {}
 
-void SettingsContainer::read(const QJsonObject &json) {}
+void SettingsContainer::read(const QJsonObject &json) {
+  mFileWatchConfig.read(json["fileWatch"].toObject());
+  mLogParserConfig.read(json["logParser"].toObject());
+  mName = json["name"].toString();
+  mSourcePath = json["sourcePath"].toString();
+}
 
-void SettingsContainer::write(QJsonObject &json) const {}
+void SettingsContainer::write(QJsonObject &json) const {
+  {
+    QJsonObject jsonObject;
+    mFileWatchConfig.write(jsonObject);
+    json["fileWatch"] = jsonObject;
+  }
+  {
+    QJsonObject jsonObject;
+    mLogParserConfig.write(jsonObject);
+    json["logParser"] = jsonObject;
+  }
+
+  json["name"] = mName;
+  json["sourcePath"] = mSourcePath;
+}
+
+QString SettingsContainer::sourcePath() const { return mSourcePath; }
+
+void SettingsContainer::setSourcePath(const QString &sourcePath) {
+  mSourcePath = sourcePath;
+}
+
+QString SettingsContainer::name() const { return mName; }
+
+void SettingsContainer::setName(const QString &name) { mName = name; }
+
+TailFileWatchConfig SettingsContainer::fileWatchConfig() const {
+  return mFileWatchConfig;
+}
+
+void SettingsContainer::setFileWatchConfig(
+    const TailFileWatchConfig &fileWatchConfig) {
+  mFileWatchConfig = fileWatchConfig;
+}
+
+RegexLogParserConfig SettingsContainer::logParserConfig() const {
+  return mLogParserConfig;
+}
+
+void SettingsContainer::setLogParserConfig(
+    const RegexLogParserConfig &logParserConfig) {
+  mLogParserConfig = logParserConfig;
+}
--- core/SettingsContainer.h
+++ core/SettingsContainer.h
@@ -1,14 +1,35 @@
 #ifndef SETTINGSCONTAINER_H
 #define SETTINGSCONTAINER_H
 
+#include "RegexLogParserConfig.h"
+#include "TailFileWatchConfig.h"
+
 #include <QJsonObject>
 
 class SettingsContainer {
  public:
   SettingsContainer();
 
-  void read(const QJsonObject& json);
-  void write(QJsonObject& json) const;
+  void read(const QJsonObject &json);
+  void write(QJsonObject &json) const;
+
+  QString sourcePath() const;
+  void setSourcePath(const QString &sourcePath);
+
+  QString name() const;
+  void setName(const QString &name);
+
+  TailFileWatchConfig fileWatchConfig() const;
+  void setFileWatchConfig(const TailFileWatchConfig &fileWatchConfig);
+
+  RegexLogParserConfig logParserConfig() const;
+  void setLogParserConfig(const RegexLogParserConfig &logParserConfig);
+
+ private:
+  TailFileWatchConfig mFileWatchConfig;
+  RegexLogParserConfig mLogParserConfig;
+  QString mSourcePath;
+  QString mName;
 };
 
 #endif  // SETTINGSCONTAINER_H
